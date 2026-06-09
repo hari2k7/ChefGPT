86af86daf2b8e31de93a6a9fe854c7263f80883a
hari2k7 <hari082007@gmail.com>
2026-06-09 23:07:03 +0530

    Add cookbook and create routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
+import Cookbook from './pages/Cookbook'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,9 +11,10 @@ function App() {
     <>
       <Navbar />
 
-      <main>
-        <Home />
-      </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cookbook" element={<Cookbook />} />
+      </Routes>
     </>
   )
 }
--- frontend/src/pages/Cookbook.jsx
+++ frontend/src/pages/Cookbook.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import api from '../services/api';
+import RecipeCard from '../components/RecipeCard';
 
 function Cookbook() {
+
+  const [recipes, setRecipes] = useState([]);
+
+  const fetchRecipes = async () => {
+    try {
+      const response =await api.get('/recipes')
+      setRecipes(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchRecipes()
+  }, [])
+
   return (
-    <div>
-        <h1>CookBook</h1>
+    <div className="max-w-5xl mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-6">
+        My Cookbook
+      </h1>
+
+      {recipes.map((recipe) => (
+        <RecipeCard
+          key={recipe._id}
+          recipe={recipe}
+        />
+      ))}
     </div>
   )
 }
